2c1c15c523188b2815ba5d950d839101e7a47b3b
Christopher Robinson <chrispaul_robinson@hotmail.com>
2018-10-22 12:03:01 -0500

    made a lot of changes, fixed a big error

--- maze.js
+++ maze.js
@@ -2,13 +2,62 @@
 // Description: Code to add functionality to a maze game web program
 
 
-window.onload;
+window.onload = function(){
+	var bound1 = document.querySelector("#boundary1");
+	var allBn = document.querySelectorAll("div.boundary");
+	var mEnd = document.getElementById("end");
+	var win = true;
+	var crashCntr = 0;
+	
+	console.log(allBn);
+	// Excercise 1
+	bound1.addEventListener("mouseover", function(){
+		this.classList.add("youlose");
+		crashCntr = crashCntr + 1;
+	})
+	
+	//Excercise 2
+	for(var i = 1; i < allBn.length; i++){
+		allBn[i].addEventListener("mouseover", function(){
+			this.classList.add("youlose");
+			crashCntr = crashCntr + 1;
+		})
+	};
+	
+	//Excercise 3
+	mEnd.addEventListener("mouseover", function(){
+		// using crashCntr
+		
+		if(crashCntr === 0){
+			alert("You Win!");
+		} else {
+			alert("YOU LOSE");
+		}
+	})
+	
 
-var bound1 = document.getElementById("boundary1");
+}
 
-var turnRed = function() {
-	bound1.classList.add("youlose");
+
+var turnRed = function(wall) {
+	wall.classList.add("youlose");
 };
 
-bound1.onmouseover.turnRed();
 
+
+/* testing for Excercise 2
+for(var i = 1; i < allBn.length; i++){
+	allBn[i].addEventListener("mouseover", function(){
+		this.classList.add("youlose");
+	})
+};
+*/
+
+//bdry1.onclick = turnRed();
+
+/*
+var test = document.getElementsByTagName('h1');
+
+console.log(test);
+test.style.backgroundColor = 'yellow';
+*/
