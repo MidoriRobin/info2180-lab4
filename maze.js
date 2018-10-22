15e05c25d16a9e0d89df7822b56f4b17f8177363
Christopher Robinson <chrispaul_robinson@hotmail.com>
2018-10-22 00:18:15 -0500

    Completion of the first excercise

---
+++ maze.js
@@ -0,0 +1,14 @@
+// Author: Christopher Robinson
+// Description: Code to add functionality to a maze game web program
+
+
+window.onload;
+
+var bound1 = document.getElementById("boundary1");
+
+var turnRed = function() {
+	bound1.classList.add("youlose");
+};
+
+bound1.onmouseover.turnRed();
+
