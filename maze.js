9bf0dcba3b6b90ab3659d50231130e963201589b
Christopher Robinson <chrispaul_robinson@hotmail.com>
2018-10-23 04:04:55 -0500

    Made all the changes

--- maze.js
+++ maze.js
@@ -6,17 +6,20 @@ window.onload = function(){
 	var bound1 = document.querySelector("#boundary1");
 	var allBn = document.querySelectorAll("div.boundary");
 	var mEnd = document.getElementById("end");
+	var mStart = document.getElementById("start"); 
+	var inst = document.getElementById("status");
+	var theMaze = document.getElementById("maze")
 	var win = true;
 	var crashCntr = 0;
 	
 	console.log(allBn);
-	// Excercise 1
+	//Exercise 1
 	bound1.addEventListener("mouseover", function(){
 		this.classList.add("youlose");
 		crashCntr = crashCntr + 1;
 	})
 	
-	//Excercise 2
+	//Exercise 2
 	for(var i = 1; i < allBn.length; i++){
 		allBn[i].addEventListener("mouseover", function(){
 			this.classList.add("youlose");
@@ -24,16 +27,49 @@ window.onload = function(){
 		})
 	};
 	
-	//Excercise 3
+	//Exercise 3
 	mEnd.addEventListener("mouseover", function(){
 		// using crashCntr
 		
+		
 		if(crashCntr === 0){
-			alert("You Win!");
+			//alert("You Win!");
+			inst.textContent = "You Win";
 		} else {
-			alert("YOU LOSE");
+			//alert("YOU LOSE");
+			inst.textContent = "YOU LOSE";
 		}
 	})
+	
+	//Exercise 4
+	mStart.addEventListener("click", function(){
+		if (bound1.classList.contains("youlose")){
+			bound1.classList.remove("youlose");
+		}
+		
+		for(var i = 1; i < allBn.length; i++){
+			if(allBn[i].classList.contains("youlose")){
+				allBn[i].classList.remove("youlose");
+			}
+		}
+		
+		crashCntr = 0;
+		inst.textContent = "Move your mouse over the \"S\" to begin.";
+		
+	})
+	
+	//Exercise 5
+	//See above
+	
+	//Exercise 6
+	theMaze.addEventListener("mouseleave", function(){
+		bound1.classList.add("youlose");
+		
+		for(var i = 1; i < allBn.length; i++){
+			allBn[i].classList.add("youlose");
+		}
+		
+	});
 	
 
 }
