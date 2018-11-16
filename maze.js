869b9317f95b74a267c0b3d7f4f69b93e54144de
Christopher Robinson <chrispaul_robinson@hotmail.com>
2018-11-16 04:05:48 -0500

    testing atom functions

--- maze.js
+++ maze.js
@@ -1,24 +1,25 @@
 // Author: Christopher Robinson
 // Description: Code to add functionality to a maze game web program
 
+//Adding new comment to test
 
 window.onload = function(){
 	var bound1 = document.querySelector("#boundary1");
 	var allBn = document.querySelectorAll("div.boundary");
 	var mEnd = document.getElementById("end");
-	var mStart = document.getElementById("start"); 
+	var mStart = document.getElementById("start");
 	var inst = document.getElementById("status");
 	var theMaze = document.getElementById("maze")
 	var win = true;
 	var crashCntr = 0;
-	
+
 	console.log(allBn);
 	//Exercise 1
 	bound1.addEventListener("mouseover", function(){
 		this.classList.add("youlose");
 		crashCntr = crashCntr + 1;
 	})
-	
+
 	//Exercise 2
 	for(var i = 1; i < allBn.length; i++){
 		allBn[i].addEventListener("mouseover", function(){
@@ -26,12 +27,12 @@ window.onload = function(){
 			crashCntr = crashCntr + 1;
 		})
 	};
-	
+
 	//Exercise 3
 	mEnd.addEventListener("mouseover", function(){
 		// using crashCntr
-		
-		
+
+
 		if(crashCntr === 0){
 			//alert("You Win!");
 			inst.textContent = "You Win";
@@ -40,37 +41,37 @@ window.onload = function(){
 			inst.textContent = "YOU LOSE";
 		}
 	})
-	
+
 	//Exercise 4
 	mStart.addEventListener("click", function(){
 		if (bound1.classList.contains("youlose")){
 			bound1.classList.remove("youlose");
 		}
-		
+
 		for(var i = 1; i < allBn.length; i++){
 			if(allBn[i].classList.contains("youlose")){
 				allBn[i].classList.remove("youlose");
 			}
 		}
-		
+
 		crashCntr = 0;
 		inst.textContent = "Move your mouse over the \"S\" to begin.";
-		
+
 	})
-	
+
 	//Exercise 5
 	//See above
-	
+
 	//Exercise 6
 	theMaze.addEventListener("mouseleave", function(){
 		bound1.classList.add("youlose");
-		
+
 		for(var i = 1; i < allBn.length; i++){
 			allBn[i].classList.add("youlose");
 		}
-		
+
 	});
-	
+
 
 }
 
